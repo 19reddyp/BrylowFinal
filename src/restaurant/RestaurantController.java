4edf517d521f5f5356564988c1d1a1eb15e3d339
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-11-19 09:41:04 -0600

    asd

--- src/restaurant/RestaurantController.java
+++ src/restaurant/RestaurantController.java
@@ -29,15 +29,23 @@ public class RestaurantController implements Initializable {
 	public RestaurantController() {
 		observableRest = FXCollections.observableArrayList();
 		// add stuff from file to restList
-		ArraySortedList<Item> menu = new ArraySortedList<Item>();
-		ArraySortedList<Review> reviews = new ArraySortedList<Review>();
-		menu.add(new Item("#8",7.85));
-		menu.add(new Item("#9",6.85));
-		reviews.add(new Review(5,"good food"));
-		reviews.add(new Review(5,"excellent experience"));
+		ArraySortedList<Item> jmenu = new ArraySortedList<Item>();
+		ArraySortedList<Review> jreviews = new ArraySortedList<Review>();
+		jmenu.add(new Item("#8",7.85));
+		jmenu.add(new Item("#9",6.85));
+		jreviews.add(new Review(5,"good food"));
+		jreviews.add(new Review(5,"excellent experience"));
 		Restaurant temp = new Restaurant(0, "JERSEY MIKE'S", "17550 W Bluemound Rd #80, Brookfield, WI 53045", "(262) 262-2626", "jersey@mikes.com",
-				"M-F 8am-9pm", "Sandwiches", "Casual", "$", menu, reviews,"restaurant/Jersey.png");
-		observableRest.add(temp);
+				"M-F 8am-9pm", "Sandwiches", "Casual", "$", jmenu, jreviews,"restaurant/Jersey.png");
+		ArraySortedList<Item> fmenu = new ArraySortedList<Item>();
+		ArraySortedList<Review> freviews = new ArraySortedList<Review>();
+		fmenu.add(new Item("Cheeseburger",9.95));
+		fmenu.add(new Item("Hamburger",8.85));
+		freviews.add(new Review(3,"good food"));
+		freviews.add(new Review(5,"excellent experience"));
+		Restaurant five = new Restaurant(1, "FIVE GUYS", "95 N. Moorland Rd, Brookfield, WI 53005", "(262) 786-2580", "five@guys.com",
+				"M-F 8am-10pm", "Burgers", "Casual", "$", fmenu, freviews,"restaurant/Guys.png");
+		observableRest.addAll(five,temp);
 	}
 
 	@Override
