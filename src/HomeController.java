fe13bfd37c765213cf0ce23548e7ddafb2e088ec
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-11-27 11:06:58 -0600

    asad

--- src/HomeController.java
+++ src/HomeController.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import database.DatabaseClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +13,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import models.ArrayUnsortedList;
+import models.Restaurant;
+import restaurant.RestaurantController;
 
 public class HomeController {
 
@@ -22,8 +26,35 @@ public class HomeController {
 	private AnchorPane root;
 
 	@FXML
+	private TextField term;
+
+	@FXML
 	private void redirect(ActionEvent event) throws IOException {
-		AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("restaurant/Restaurants.fxml"));
+		FXMLLoader page = new FXMLLoader(getClass().getResource("restaurant/Restaurants.fxml"));
+		AnchorPane pane = page.load();
+		DatabaseClass data = new DatabaseClass();
+		ArrayUnsortedList<Restaurant> rest = data.getRestaraunts();
+		RestaurantController controller = page.getController();
+		controller.setList(rest);
+		root.getChildren().setAll(pane);
+	}
+
+	@FXML
+	public void search() throws IOException {
+		String searchTerm = term.getText();
+		DatabaseClass data = new DatabaseClass();
+		ArrayUnsortedList<Restaurant> restaurants = data.getRestaraunts();
+		ArrayUnsortedList<Restaurant> fit = new ArrayUnsortedList<Restaurant>();
+		for (int x = 0; x < restaurants.size(); x++) {
+			Restaurant temp = restaurants.getNext();
+			if(temp.getName().toLowerCase().contains(searchTerm.toLowerCase())) {
+				fit.add(temp);
+			}
+		}
+		FXMLLoader page = new FXMLLoader(getClass().getResource("restaurant/Restaurants.fxml"));
+		AnchorPane pane = page.load();
+		RestaurantController controller = page.getController();
+		controller.setList(fit);
 		root.getChildren().setAll(pane);
 	}
 }
